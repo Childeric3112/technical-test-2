057dd58eab3705b8b6196e756d6a8014bea23eda
Childeric <corentinbozecpro@gmail.com>
2025-03-17 13:42:55 +0100

    [FIX] project

--- app/src/scenes/project/view.js
+++ app/src/scenes/project/view.js
@@ -28,6 +28,12 @@ export default function ProjectView() {
     })();
   }, []);
 
+  // const project = React.useMemo(async () => {
+  //   const { data: u } = await api.get(`/project/${id}`);
+  //   return u?u:null;
+  //   }, [id]
+  // )
+
   useEffect(() => {
     if (copied) {
       setTimeout(() => setCopied(false), 3000);
@@ -60,7 +66,6 @@ export default function ProjectView() {
 }
 
 const ProjectDetails = ({ project }) => {
-  console.log(project);
   return (
     <div>
       <div className="flex flex-wrap p-3">
@@ -70,7 +75,7 @@ const ProjectDetails = ({ project }) => {
               <div className="flex justify-between gap-2">
                 <div className="flex gap-20">
                   <span className="w-fit text-[20px] text-[#0C1024] font-bold">Nom du projet : </span>
-                  <span className="w-fit text-[20px] text-[#0C1024] font-bold">{project.name.toString()}</span>
+                  <span className="w-fit text-[20px] text-[#0C1024] font-bold">{project.name?.toString()}</span>
                 </div>
                 <div className="flex flex-1 flex-column items-end gap-3">
                   <Links project={project} />
